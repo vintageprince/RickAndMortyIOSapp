7761f9ac840b9342b8290328f85c86420399fc1b
vintageprince <memories9843@gmail.com>
2024-01-22 16:13:28 +0200

    Build out character detail screen

---
+++ RickAndMortyApp/ViewModels/CharacterDetail/RMCharacterPhotoCollectionViewCellViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  RMCharacterPhotoCollectionViewCellViewModel.swift
+//  RickAndMortyApp
+//
+//  Created by Даниил Заканавский on 17.01.2024.
+//
+
+import Foundation
+
+final class RMCharacterPhotoCollectionViewCellViewModel {
+    private let imageUrl: URL?
+    
+    init(imageUrl: URL?) {
+        self.imageUrl = imageUrl
+    }
+    
+    public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let imageUrl = imageUrl else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        RMImageLoader.shared.downloadImage(imageUrl, completion: completion)
+    }
+}
